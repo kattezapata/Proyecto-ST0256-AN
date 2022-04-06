0dc21bd96aed1b30c89dcf2a69b48cd91479bc77
kattezapata <68928386+kattezapata@users.noreply.github.com>
2022-04-06 16:06:34 +0000

    Update

---
+++ metodos_ecuaciones/secante.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def secante(f, x_i, dx, max_ite):
+    if f(x_i) == 0:
+        print(f"{x_i} es raíz la función f(x)")
+    else:
+        ite = 0
+        x_f = x_i + dx
+        while(f(x_i)*f(x_f) > 0 and ite < max_ite):
+            x_i = x_f
+            x_f = x_i + dx
+            ite += 1
+          
+        if f(x_i)*f(x_f) == 0:
+            print(f"{x_i} es raíz la función f(x)")
+            print(f"{x_i} es raíz con tolerancia {tol}")
+        elif f(x_i)*f(x_f) < 0:
+            print("Entre "f"{x_i} y "f"{x_f} hay raíz de la función f(x)")
+        else:
+            print("En "f"{max_ite} no se encontró raíz para la función f(x)")
+    return
+
+f = lambda x : np.sin(x)
+
+secante(f, 0, 0.01, 100)
