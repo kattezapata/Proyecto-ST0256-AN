ffb434c1ea7cf320250986f7285a5a2c1f1e1ac8
kattezapata <68928386+kattezapata@users.noreply.github.com>
2022-05-31 03:06:52 -0500

    Add files via upload

--- numérico/main.py
+++ numérico/main.py
@@ -1,5 +1,7 @@
 
 #import numpy as np
+from ast import Yield
+from cmath import exp
 import sys
 import tkinter as tk
 from tkinter import *
@@ -8,10 +10,15 @@ from ecuaciones import busquedas
 
 def hacer_click():
  try:
-  _valor = int(entrada_texto.get())
-  f = lambda x : x
+  _x = int(entrada_texto.get())
+  _f=entrada_texto2.get()
+  _dx=float(entrada_texto3.get())
+  _max=int(entrada_texto4.get())
+  #_f= eval(entrada_texto2.get())
+  
+  #f = lambda x : x
  # f = lambda x : np.sin(x)
-  _valor =  busquedas.busquedas(f,_valor, 0.01, 100)
+  _valor =  busquedas.busquedas(_f,_x, _dx, _max)
   etiqueta.config(text=_valor)
  except ValueError:
   etiqueta.config(text="Resultado")
@@ -28,18 +35,37 @@ vp.columnconfigure(0, weight=1)
 vp.rowconfigure(0, weight=1)
 
 etiqueta = Label(vp, text="Resultado")
-etiqueta.grid(column=2, row=2, sticky=(W,E))
+etiqueta.grid(column=2, row=6, sticky=(W,E))
 
 boton = Label(vp, text="X inicial")
 boton.grid(column=1, row=1)
 
-bot= Button(vp, text="OK!", command=hacer_click)
-bot.grid(column=3, row=1)
+boton2 = Label(vp, text="Función f en términos de x")
+boton2.grid(column=1, row=2)
 
-valor = ""
-entrada_texto = Entry(vp, width=10, textvariable=valor)
+boton3 = Label(vp, text="dx")
+boton3.grid(column=1, row=3)
+
+boton4 = Label(vp, text="´Máximo iterador")
+boton4.grid(column=1, row=4)
+
+bot= Button(vp, text="OK!", command=hacer_click)
+bot.grid(column=4, row=1)
+
+valor1 = ""
+entrada_texto = Entry(vp, width=10, textvariable=valor1)
 entrada_texto.grid(column=2, row=1)
 
+valor2 = ""
+entrada_texto2 = Entry(vp, width=10, textvariable=valor2)
+entrada_texto2.grid(column=2, row=2)
 
+valor3 = ""
+entrada_texto3 = Entry(vp, width=10, textvariable=valor3)
+entrada_texto3.grid(column=2, row=3)
+
+valor4 = ""
+entrada_texto4 = Entry(vp, width=10, textvariable=valor4)
+entrada_texto4.grid(column=2, row=4)
 
 app.mainloop()
