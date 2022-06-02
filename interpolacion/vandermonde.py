66b46e5b21db9ee26eafb8873b6b42e05631060c
Mariana13-31 <68928431+Mariana13-31@users.noreply.github.com>
2022-06-02 12:13:23 -0500

    Delete interpolacion directory

--- interpolacion/vandermonde.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def vandermonde(x,y):
-    n = len(x)
-    M = np.zeros((n,n))
-    for i in range(len(x)):
-        for j in range(len(x)):
-            M[i][j] = x[i]**j
-    return np.linalg.inv(M) @ y
